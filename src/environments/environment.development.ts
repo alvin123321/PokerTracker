f2d5c9ab2d82f1778ef2d88941ebb420ff329dd0
alvin123321 <alvin123321@users.noreply.github.com>
2026-06-26 10:26:49 -0700

    chore: configure Supabase environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://rfyaqfecnkwrlcdfmmaj.supabase.co',
+  supabaseAnonKey: 'sb_publishable_0mE-I-s66WY11J5QJFG8Mg_lJPWAYrw'
 };
